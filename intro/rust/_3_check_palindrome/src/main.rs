938809491288d22c0a048d3dcc4e60aae58c1730
George Richards <georich@protonmail.com>
2018-05-17 11:56:34 +0100

    Rust checkPalindrome solution

--- intro/rust/_3_check_palindrome/src/main.rs
+++ intro/rust/_3_check_palindrome/src/main.rs
@@ -1,3 +1,10 @@
+fn check_palindrome(input: &String) -> bool {
+    let half_length = input.len() / 2;
+    input.chars().take(half_length).eq(input.chars().rev().take(half_length))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let word = String::from("aabaa");
+    let result = check_palindrome(&word);
+    println!("Is {} a palindrome? {} ", word, result);
 }
