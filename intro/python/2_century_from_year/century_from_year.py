b35459abf50d3d60f4dab7bad0a4560608e0494d
George Richards <georich@protonmail.com>
2018-06-06 10:20:31 +0100

    Clean up code

--- intro/python/2_century_from_year/century_from_year.py
+++ intro/python/2_century_from_year/century_from_year.py
@@ -5,5 +5,4 @@ def century_from_year(year: int) -> int:
     """Figure out which century a given year would be in."""
     if year % 100 == 0:
         return year // 100
-    else:
-        return (year // 100) + 1
+    return (year // 100) + 1
--- intro/python/3_check_palindrome/check_palindrome.py
+++ intro/python/3_check_palindrome/check_palindrome.py
@@ -1,8 +1,8 @@
 """Solution for checkPalindrome."""
 
 
-def check_palindrome(input: str) -> bool:
+def check_palindrome(input_string: str) -> bool:
     """Check if a given str is a palindrome or not, returning true or false."""
-    if input == input[::-1]:
+    if input_string == input_string[::-1]:
         return True
     return False
